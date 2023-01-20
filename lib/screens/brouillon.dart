983758e4f8724c5d4d3712d59d5063a5f5aa94c6
UserEstrella <stellanyarko37@gmail.com>
2023-01-20 17:27:29 +0000

    Ajout d'une fonction de Googles maps

--- lib/screens/brouillon.dart
+++ lib/screens/brouillon.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapsSceen extends StatefulWidget {
   const MapsSceen({Key? key}) : super(key: key);
@@ -9,10 +9,46 @@ class MapsSceen extends StatefulWidget {
 }
 
 class _MapsSceenState extends State<MapsSceen> {
+  static const LatLng currentLocation = LatLng(5.3510144, -3.997696);
+  late GoogleMapController _mapController;
+  final Map<String, Marker> _markers = {};
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Google Maps example'),
+      ),
+      body: GoogleMap(
+          initialCameraPosition: const CameraPosition(
+              target: currentLocation,
+            zoom: 14
+          ),
+        onMapCreated: (controller){
+          _mapController = controller;
 
+          addMarker('wineshop', currentLocation);
+        },
+        markers: _markers.values.toSet(),
+      ),
     );
+  }
+  
+  addMarker(String id, LatLng location) async{
+    var markerIcon = await BitmapDescriptor.fromAssetImage(const ImageConfiguration(), "assets/wine.png");
+    var marker = Marker(
+        markerId: MarkerId(id),
+      position: location,
+      infoWindow: const InfoWindow(
+        title: 'Title of place',
+        snippet: "Some description of the place",
+      ),
+      icon: markerIcon,
+    );
+
+    _markers[id] = marker;
+    setState(() {
+
+    });
   }
 }
