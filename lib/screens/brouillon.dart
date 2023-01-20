cbb3562685d7817f85880cff3f9d2cad2c0d0e37
UserEstrella <stellanyarko37@gmail.com>
2023-01-20 16:51:20 +0000

    Add du package google maps

---
+++ lib/screens/brouillon.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MapsSceen extends StatefulWidget {
+  const MapsSceen({Key? key}) : super(key: key);
+
+  @override
+  State<MapsSceen> createState() => _MapsSceenState();
+}
+
+class _MapsSceenState extends State<MapsSceen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
